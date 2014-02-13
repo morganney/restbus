481ece46df7c75099c84c364dda62804078b7617
Morgan Ney <morganney@gmail.com>
2014-02-13 00:29:02 -0800

    tested demo mode and new methods.

--- test/restbus.js
+++ test/restbus.js
@@ -1,3 +1,25 @@
 var restbus = require('../index');
 
-restbus.listen();
+restbus.listen(function(port) {
+  console.log('restbus listening on port ' + port);
+});
+
+setTimeout(function() {
+ restbus.close(function() {
+   console.log('restbus went to bed for the night');
+ });
+}, 10000);
+
+/*
+(function recurse() {
+
+  if(restbus.isListening()) {
+    restbus.close(function() {console.log('restbus is not listening.')});
+  } else {
+    restbus.listen('3535', function() {console.log('restbus is now listening on port 3535.')}, true);
+  }
+
+  setTimeout(recurse,10000);
+
+}());
+*/
